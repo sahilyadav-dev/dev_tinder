85061553ca4f6384c1b6bdba98cef721689c37fd
sahilyadav-dev <sahilyadav29399@gmail.com>
2025-08-19 00:27:20 +0530

    explored HTTP requests

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ const app = express();
 app.listen(3000 , () => {
   console.log('server is up')
 });
-
-// app.use('/', (req,res) => {
-//   res.send('hello from server');
-// })
-app.use('/hello', (req,res) => {
+app.get("/userdata",(req,res) => {
+  res.send({name: 'sahil', age: 24})
+})
+app.delete("/delete",(req,res) => {
+  res.send("Your Data Deleted")
+})
+app.post('/hello/2', (req,res) => {
+  res.send("aabra ka daabra")
+})
+app.get('/hello', (req,res) => {
   res.send("hello hello hello!");
 })
 app.use('/profile', (req,res) => {
   res.send('this is your profile');
+})
+app.use('/', (req,res) => {
+  res.send('hello from server');
 })
