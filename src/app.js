271874638f6cfe2ea0efc3684fd77f98d5867dad
sahilyadav-dev <sahilyadav29399@gmail.com>
2025-08-26 21:34:36 +0530

    created different APIs

--- src/app.js
+++ src/app.js
@@ -26,6 +26,32 @@ app.post("/signup", async (req,res) => {
   }
 });
 
+app.patch('/user/update',async (req,res) => {
+  const id = req.body._id
+  console.log(id)
+
+  try{
+    const user = await User.findByIdAndUpdate(id,{lastName: 'rakuuuuuu'},{firstName: 'raku'});
+    res.send('user updated');
+  }
+  catch (err){
+    res.status(400).send('not found');
+  }
+})
+
+app.get("/user/id",async (req,res) => {
+  const id = req.body._id;
+  console.log(id)
+
+  try{
+    const user = await User.findById(id);
+    res.send(user)
+  } 
+  catch (err){
+    res.status(400).send('not found')
+  }
+})
+
 app.get('/user', async (req,res) => {
   const userEmail = req.body.emailId;
   console.log(userEmail)
@@ -51,3 +77,30 @@ app.get("/feed",async (req,res) => {
     res.status(400).send('not found')
   }
 })
+
+app.delete('/delete/id',async (req,res) => {
+  const id = req.body.firstName
+  console.log(id)
+
+  try{
+    const user = await User.findOneAndDelete({firstName: id});
+    res.send('user deleted');
+  }
+  catch (err){
+    res.status(400).send('not found');
+  }
+})
+
+app.delete('/delete',async (req,res) => {
+  const id = req.body.emailId
+  console.log(id)
+
+  try{
+    const user = await User.deleteOne({emailId: id});
+    res.send('user deleted');
+  }
+  catch (err){
+    res.status(400).send('not found');
+  }
+})
+
